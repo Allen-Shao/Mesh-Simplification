8461fff0bfede071d7f76c4fa537991d38e016ef
Allen-Shao <shaoyiyang312@hotmail.com>
2017-03-02 16:28:19 +0800

    Generalize the usage

--- Decimation.cxx
+++ Decimation.cxx
@@ -25,12 +25,12 @@ vtkSmartPointer<vtkPolyData> Subdivision(vtkSmartPointer<vtkPolyData> input, int
 int main(int argc, char *argv[])
 {
   if (argc != 6){
-    cout << "ExampleCommand: $ ./qslim -t 1 -o <output filename> <input filename>" << endl;
+    cout << "ExampleCommand: $ ./simplification -n <target no. of polygons> -o <output filename> <input filename>" << endl;
     return EXIT_FAILURE;
   } 
       
   std::cout << "Reading file..." << std::endl;
-  int targetNumberOfPolygons = 1000;
+  int targetNumberOfPolygons = atoi(argv[2]);
   std::string inputFilename = argv[5];
   std::string outputFilename = argv[4];
   vtkSmartPointer<vtkSMFReader> reader = 
@@ -82,6 +82,68 @@ int main(int argc, char *argv[])
   writer->SetInputData(decimated);
   writer->Update();
 
+  //Visualization
+  vtkSmartPointer<vtkPolyDataMapper> inputMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+#if VTK_MAJOR_VERSION <= 5
+  inputMapper->SetInputConnection(input->GetProducerPort());
+#else
+  inputMapper->SetInputData(input);
+#endif
+  vtkSmartPointer<vtkActor> inputActor =
+    vtkSmartPointer<vtkActor>::New();
+  inputActor->SetMapper(inputMapper);
+
+  vtkSmartPointer<vtkPolyDataMapper> decimatedMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+#if VTK_MAJOR_VERSION <= 5
+  decimatedMapper->SetInputConnection(decimated->GetProducerPort());
+#else
+  decimatedMapper->SetInputData(decimated);
+#endif
+  vtkSmartPointer<vtkActor> decimatedActor =
+    vtkSmartPointer<vtkActor>::New();
+  decimatedActor->SetMapper(decimatedMapper);
+
+  // There will be one render window
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->SetSize(600, 300);
+
+  // And one interactor
+  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  interactor->SetRenderWindow(renderWindow);
+
+  // Define viewport ranges
+  // (xmin, ymin, xmax, ymax)
+  double leftViewport[4] = {0.0, 0.0, 0.5, 1.0};
+  double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
+
+  // Setup both renderers
+  vtkSmartPointer<vtkRenderer> leftRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  renderWindow->AddRenderer(leftRenderer);
+  leftRenderer->SetViewport(leftViewport);
+  leftRenderer->SetBackground(.6, .5, .4);
+
+  vtkSmartPointer<vtkRenderer> rightRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  renderWindow->AddRenderer(rightRenderer);
+  rightRenderer->SetViewport(rightViewport);
+  rightRenderer->SetBackground(.4, .5, .6);
+
+  // Add the sphere to the left and the cube to the right
+  leftRenderer->AddActor(inputActor);
+  rightRenderer->AddActor(decimatedActor);
+
+  leftRenderer->ResetCamera();
+
+  rightRenderer->ResetCamera();
+
+  renderWindow->Render();
+  interactor->Start();
+
 
   return EXIT_SUCCESS;
 
@@ -114,72 +176,4 @@ vtkSmartPointer<vtkPolyData> Subdivision(vtkSmartPointer<vtkPolyData> input, int
   return subdivisionFilter->GetOutput();
 }
 
-
-
-
-
-
-//Visualization
-//   vtkSmartPointer<vtkPolyDataMapper> inputMapper =
-//     vtkSmartPointer<vtkPolyDataMapper>::New();
-// #if VTK_MAJOR_VERSION <= 5
-//   inputMapper->SetInputConnection(input->GetProducerPort());
-// #else
-//   inputMapper->SetInputData(input);
-// #endif
-//   vtkSmartPointer<vtkActor> inputActor =
-//     vtkSmartPointer<vtkActor>::New();
-//   inputActor->SetMapper(inputMapper);
-
-//   vtkSmartPointer<vtkPolyDataMapper> decimatedMapper =
-//     vtkSmartPointer<vtkPolyDataMapper>::New();
-// #if VTK_MAJOR_VERSION <= 5
-//   decimatedMapper->SetInputConnection(decimated->GetProducerPort());
-// #else
-//   decimatedMapper->SetInputData(decimated);
-// #endif
-//   vtkSmartPointer<vtkActor> decimatedActor =
-//     vtkSmartPointer<vtkActor>::New();
-//   decimatedActor->SetMapper(decimatedMapper);
-
-//   // There will be one render window
-//   vtkSmartPointer<vtkRenderWindow> renderWindow =
-//     vtkSmartPointer<vtkRenderWindow>::New();
-//   renderWindow->SetSize(600, 300);
-
-//   // And one interactor
-//   vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-//     vtkSmartPointer<vtkRenderWindowInteractor>::New();
-//   interactor->SetRenderWindow(renderWindow);
-
-//   // Define viewport ranges
-//   // (xmin, ymin, xmax, ymax)
-//   double leftViewport[4] = {0.0, 0.0, 0.5, 1.0};
-//   double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
-
-//   // Setup both renderers
-//   vtkSmartPointer<vtkRenderer> leftRenderer =
-//     vtkSmartPointer<vtkRenderer>::New();
-//   renderWindow->AddRenderer(leftRenderer);
-//   leftRenderer->SetViewport(leftViewport);
-//   leftRenderer->SetBackground(.6, .5, .4);
-
-//   vtkSmartPointer<vtkRenderer> rightRenderer =
-//     vtkSmartPointer<vtkRenderer>::New();
-//   renderWindow->AddRenderer(rightRenderer);
-//   rightRenderer->SetViewport(rightViewport);
-//   rightRenderer->SetBackground(.4, .5, .6);
-
-//   // Add the sphere to the left and the cube to the right
-//   leftRenderer->AddActor(inputActor);
-//   rightRenderer->AddActor(decimatedActor);
-
-//   leftRenderer->ResetCamera();
-
-//   rightRenderer->ResetCamera();
-
-//   renderWindow->Render();
-//   interactor->Start();
-  
-//   return EXIT_SUCCESS;
 
